7db5248aee844238bf914a5d2746bb9e103ba2c3
raindevops <raindevops.durand@gmail.com>
2021-11-18 11:18:39 +0100

    refacto layout & generate top 200 albums

--- crawler.py
+++ crawler.py
@@ -27,37 +27,25 @@ def get_page_source(url, filename):
     file_source.close()
     return str(body)
     
-def start_crawling(soup, filename):
-    extract_products(soup, filename)
-    # pagination = soup.find('ul',{'class': 'pagination-custom'})
-    # next_page = pagination.find_all('li')[-1]
-    # if next_page.has_attr('class'):
-    #     if next_page['class'] == ['disabled']:
-    #         print("You reached the last page, stopping crawler...")
-    # else:
-    #     next_page_link = next_page.find('a')['href']
-    #     next_page_address = BASE_URL + next_page_link
-    #     next_page_index = next_page_link[next_page_link.find('=') + 1 ]
-    #     crawl(next_page_address, f'etee-page{next_page_index}.txt')
 
 def extract_products(soup, filename):
     csv_filename = filename.replace('.txt','.csv')
-    products_file = open(f'products/{csv_filename}', mode='a', encoding='utf-8', newline='')
+    products_file = open(f'musics/metal/{csv_filename}', mode='a', encoding='utf-8', newline='')
     products_writer = csv.writer(products_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     products_writer.writerow(['Band', 'Album', 'Year'])
     products = soup.find_all('table', {'class': 'table table-compact table-striped'})[0].find('tbody')
-
 
     for product in products:
         base = product.find('td',{'class':'', 'width':'', 'align':''})
         band = base.find('b').find('a').getText()
         albums = base.find('div',{'class':'visible-xs'}).find('a').getText()
         years = base.find('div',{'class':'visible-xs'}).find('span').getText()
+        products_writer.writerow([band,albums,years])
 
 def crawl(url, filename):
     page_body = get_page_source(url, filename)
     soup = BeautifulSoup(page_body, 'html.parser')
-    start_crawling(soup, filename)
+    extract_products(soup, filename)
 
-crawl(FULL_START_URL,'etee-page1.txt')
+crawl(FULL_START_URL,'crawl-top-200.txt')
 
