8c48c0e9fad9c96f395e08d1b6fad7a1c104d951
raindevops <raindevops.durand@gmail.com>
2021-11-18 09:24:14 +0100

    pokv1

--- test.py
+++ test.py
@@ -5,11 +5,3 @@ videosSearch = VideosSearch('OrelSan', limit = 1).result()['result']
 for video in videosSearch:
     print(video['link'])
 
-
-
-# from youtubesearchpython import PlaylistsSearch
-
-# search = PlaylistsSearch('erutan raindancer', limit=12)
-# playlist = search.result()
-# for video in playlist['result']:
-#     print(video['title'])
