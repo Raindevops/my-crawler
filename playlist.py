35f2fe362b1ec2e541df265ebe724585b34d9685
raindevops <raindevops.durand@gmail.com>
2021-11-18 13:31:17 +0100

    create folder layout dynamically & download playlists in dedicated folder

--- playlist.py
+++ playlist.py
@@ -1,5 +1,7 @@
 from youtubesearchpython import PlaylistsSearch
 import csv
+import subprocess
+import os
 
 # Get data from CSV
 # Search the playlist on yt
@@ -14,7 +16,6 @@ with open('musics/metal/crawl-top-200.csv', 'r') as file:
 
 def get_playlist_url():
 
-    print(playlist)
     search = []
     for item in playlist:
         search.append(PlaylistsSearch( item, limit=1).result())
@@ -27,6 +28,25 @@ def get_playlist_url():
 
     return links
 
-urls = get_playlist_url()
 
-print(urls)
+def download_playlists():
+    urls = get_playlist_url()
+    index = 0
+    for url in urls:
+        if not os.path.exists('musics/metal/' + playlist[index]):
+            os.makedirs('musics/metal/' + playlist[index])
+        subprocess.run([
+                'youtube-dl',
+                url ,
+                '-x', 
+                '--audio-format', 
+                'mp3', 
+                '--audio-quality', 
+                '0', 
+                '--yes-playlist',
+                '-o',
+                "musics/metal/" + playlist[index] +'/%(title)s-%(id)s.%(ext)s'
+            ])
+        index+=1
+
+download_playlists()
